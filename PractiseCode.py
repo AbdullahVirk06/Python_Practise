38295944f2f1e72003922b1000ff3fe2ac228049
Muhammad Abdullah <whello544@gmail.com>
2026-03-15 20:35:20 +0500

    Duplicate variable definitions and print statements
    Added duplicate variable definitions and print statements for debugging.

--- PractiseCode.py
+++ PractiseCode.py
@@ -19,3 +19,18 @@ address += ', Apartment 4B'
 employee_age = 28
 employee_info = full_name + ' is ' + str(employee_age) + ' years old'
 print(employee_info)
+
+###3/15/2026
+
+name = 'Alice'
+print(name, type(name))
+
+is_student = True
+print(is_student, type(is_student))
+
+age = 20
+print(age, type(age))
+
+score = 80.5
+print(isinstance(score, float))
+print(score,type(score))
